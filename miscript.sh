e9ddb13889660b264798bae9215efacdf13c7cfb
Víctor Morales Flores <78635643+VictorMoralesFlores@users.noreply.github.com>
2021-10-05 12:09:54 -0500

    Create miscript.sh
    adding function to a script

---
+++ miscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+add_a_user
+{
+    USER = $1
+    PASSWORD = $2
+    shift;shift;
+    COMMENTS=$@
+    echo "Adding user $USER ..."
+    echo useradd -c "$COMMENTS" $USER
+    echo passwd $USER $PASSWORD
+    echo "Added user $USER ($COMMENTS) with pass $PASSWORD"
+}
+
+
+echo "Comienza el script"
+add_a_user bob sanchez Bob el constructor
+add_a_user alex badpassword Alex Pumas gol
+add_a_user bilko peorpassword Bilko just an example
+echo "Final del script..."
+echo "Paz y amor"
